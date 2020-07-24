f7d6359c4a0744322c4031493d40120d10434033
muhammadzee343 <muhammad.zee343@gmail.com>
2020-07-24 11:58:00 +0500

    fresh project

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Authentication firebase</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
